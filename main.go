441ace88ce04ea750af814db894fbe3b73aab69b
Kadu Chessi <kaduchessi@gmail.com>
2024-03-18 20:21:41 -0300

    Add new lines

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p Person) Walk() {
+	fmt.Println(p.name + " is walking")
+}
+
 func main() {
-	print("Hello, World")
+	var a int
+	a = 10
+	fmt.Println(a)
+
+	// array
+	// var b [5]int
+
+	// slice
+	// var c []int
+
+	var pessoa Person
+	pessoa.name = "Chessi"
+	pessoa.age = 32
+
+	fmt.Println(pessoa.name + " is " + string(pessoa.age) + " years old")
+
+	pessoa.Walk()
 }
